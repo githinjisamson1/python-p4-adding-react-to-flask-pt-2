c183cd511368e0731e4f55c236d16f4a7d29489e
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-14 05:07:19 +0300

    Completed assignment

--- server/seed.py
+++ server/seed.py
@@ -9,10 +9,11 @@ from models import db, Movie
 
 fake = Faker()
 
+
 def make_movies():
 
     Movie.query.delete()
-    
+
     movies = []
     for i in range(50):
         m = Movie(title=fake.sentence(nb_words=4).title())
@@ -20,6 +21,7 @@ def make_movies():
 
     db.session.add_all(movies)
     db.session.commit()
+
 
 if __name__ == '__main__':
     with app.app_context():
